eb311c1ee52d553816aedd8b28c4323e070b82b5
Yazan-Hage <yazanhage67@gmail.com>
2025-06-27 07:24:57 -0700

    Delete Constant directory

--- Constant/main_constants.dart
+++
@@ -1,7 +0,0 @@
-
-class APIs{
-  static const String authUrl = 'http://192.168.40.168:8000/api/auth.php';
-  static const String requestUrl = 'http://192.168.40.168:8000/api/requests.php';
-  static const String locationUserUrl =
-      'http://192.168.40.168:8000/api/user_locations.php';
-}
--- Constant/name_of_pages.dart
+++
@@ -1,7 +0,0 @@
-class Screens {
-  static const String introScreen = '/intro';
-  static const String splashScreen = '/splash';
-  static const String logInScreen = '/login';
-  static const String registerScreen = '/register';
-  static const String homeScreen = '/home';
-}
